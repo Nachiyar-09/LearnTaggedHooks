795f67b01d172310d054586d6c18707377cca4f4
Nachiyar-09 <85991689+Nachiyar-09@users.noreply.github.com>
2021-09-17 20:25:23 +0530

    Added Driver factory page

---
+++ src/test/java/com/DriverFactory/DriverClass.java
@@ -0,0 +1,34 @@
+package DriverFactory;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverClass {
+    public WebDriver driver;
+
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+    public WebDriver init_driver(String browser) {
+
+        System.out.println("browser value is: " + browser);
+
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            tlDriver.set(new ChromeDriver());
+        } else if (browser.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            tlDriver.set(new FirefoxDriver());
+        } else {
+            System.out.println("Please pass the correct browser value: " + browser);
+        }
+
+        getDriver().manage().deleteAllCookies();
+        getDriver().manage().window().maximize();
+        return getDriver();
+
+    }
+    public static synchronized WebDriver getDriver() {
+        return tlDriver.get();
+    }
+}
